f3951ad0298c8e00afa74a090c564d0ab23aa553
Adriaan van der Bergh <avanderbergh@gmail.com>
2019-09-29 20:32:57 +0200

    Update checker

--- part1/code.js
+++ part1/code.js
@@ -2,7 +2,7 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 function encode(message, key) {
   let encodedMessage = "";
-  for (let i = 0; i < message.lengh; i++) {
+  for (let i = 0; i < message.length; i++) {
     let letter = message.charAt(i);
     let letterNumber = alphabet.indexOf(letter);
     letterNumber += key;
--- part1/referee.js
+++ part1/referee.js
@@ -1,6 +1,19 @@
+function decode(message, key) {
+  let decodedMessage = "";
+  for (let i = 0; i < message.length; i++) {
+    let letter = message.charAt(i);
+    let letterNumber = alphabet.indexOf(letter);
+    letterNumber -= key;
+    if (letterNumber < 0) letterNumber += alphabet.length;
+    decodedMessage += alphabet.charAt(letterNumber);
+  }
+  return decodedMessage;
+}
+
 function checkEncoding(encode) {
-  let encodedMessage = encode("hello", 7);
-  if (encodedMessage == "olssv") {
+  let testMessage = "abc";
+  let key = 1;
+  if (decode(encode(testMessage, key), key) == testMessage) {
     console.log("TECHIO> success true");
   } else {
     console.log("TECHIO> success false");
