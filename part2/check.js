98b8cd85a33a88dce00cf19f49693332f7b876d6
Adriaan van der Bergh <vanderbergha@VANDERBERGHA-LP01.local>
2019-09-30 09:26:13 +0200

    Fixed checker

--- part2/check.js
+++ part2/check.js
@@ -1,15 +1,15 @@
 let expectedSum = 0;
  
-for (i = 1; i <= 100; i += 5) {
+for (i = 0; i <= 100; i += 5) {
     expectedSum += i
 }
 
 let sum = require("./code");
 
 if (sum == expectedSum) {
-    console.log("TECHIO> status true");
+    console.log("TECHIO> success true");
     console.log("TECHIO> message Correct!")
 } else {
-    console.log("TECHIO> status false");
+    console.log("TECHIO> success false");
     console.log("TECHIO> message Incorrect! Sum is " + sum)
 }
