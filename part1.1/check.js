247390dc1c7ccfb2a876a0ca903fa2fb4e789f65
Adriaan van der Bergh <vanderbergha@VANDERBERGHA-LP01.local>
2019-09-30 10:34:07 +0200

    Fixed success called twice

--- part1.1/check.js
+++ part1.1/check.js
@@ -1,17 +1,16 @@
+let success = true;
+
 const myString = "dQbQCMYkgMCiwHEZ4MGIU2JPmmSFQyCc3hnL4iqxvMm3CjLMAHSTUtuYygrs8J5twktpkAwcCYuL27EHJLhgdhUqbvvpRCeDFu0D"
 
 const values = require("./code");
 
 console.log("TECHIO> message your character is " + values.myChar)
 
-if (values.myChar == myString.charAt(60)) {
-    console.log("TECHIO> success true");
-} else {
-    console.log("TECHIO> success false");
+if (values.myChar != myString.charAt(60)) {
+    success = false
+} 
+if (values.position != myString.indexOf("j") + 1) {
+    success = false
 }
 
-if (values.position == myString.indexOf("j") + 1) {
-    console.log("TECHIO> success true");
-} else {
-    console.log("TECHIO> success false");
-}
+console.log("TECHIO> success " + String(success))
