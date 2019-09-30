0a5194dd1d265f196c061eeb69db1a69e38eef8d
Adriaan van der Bergh <vanderbergha@VANDERBERGHA-LP01.local>
2019-09-30 09:29:35 +0200

    Fixed expected sum

--- part2/check.js
+++ part2/check.js
@@ -1,15 +1,15 @@
 let expectedSum = 0;
- 
-for (i = 0; i <= 100; i += 5) {
-    expectedSum += i
+
+for (i = 0; i <= 50; i += 5) {
+  expectedSum += i;
 }
 
 let sum = require("./code");
 
 if (sum == expectedSum) {
-    console.log("TECHIO> success true");
-    console.log("TECHIO> message Correct!")
+  console.log("TECHIO> success true");
+  console.log("TECHIO> message Correct! The sum is " + sum);
 } else {
-    console.log("TECHIO> success false");
-    console.log("TECHIO> message Incorrect! Sum is " + sum + " should be " + expectedSum)
+  console.log("TECHIO> success false");
+  console.log("TECHIO> message Your sum (" + sum + ") is incorrect!");
 }
