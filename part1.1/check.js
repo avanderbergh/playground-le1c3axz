c0a75fc4f9bbafbffd88c2313070795b14221f52
Adriaan van der Bergh <vanderbergha@VANDERBERGHA-LP01.local>
2019-09-30 13:39:36 +0200

    Add output checking

--- part1.1/check.js
+++ part1.1/check.js
@@ -4,15 +4,16 @@ const myString = "dQbQCMYkgMCiwHEZ4MGIU2JPmmSFQyCc3hnL4iqxvMm3CjLMAHSTUtuYygrs8J
 
 const values = require("./code");
 
-console.log("TECHIO> message your character is " + values.myChar)
+let correctChar = myString.charAt(59 + 1);
+let correctPosition = myString.indexOf("j") + 1
 
-console.log("TECHIO> message myChar :" + values.myChar + " position: " + values.position)
-
-if (values.myChar != myString.charAt(60)) {
+if (values.myChar != correctChar) {
     success = false
 } 
-if (values.position != myString.indexOf("j") + 1) {
+if (values.position != correctPosition) {
     success = false
 }
 
 console.log("TECHIO> success " + String(success))
+console.log("TECHIO> message myChar:" + values.myChar + " should be: " + correctChar);
+console.log("TECHIO> message position:" + values.position + " should be: " + correctPosition);
