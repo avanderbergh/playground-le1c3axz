169fd8db0255178a06ca39a0412519509575ad8f
Adriaan van der Bergh <vanderbergha@VANDERBERGHA-LP01.local>
2019-10-01 09:43:56 +0200

    Fixed a bug in part 1.1

--- part1.1/code.js
+++ part1.1/code.js
@@ -1,5 +1,5 @@
 const myString = "dQbQCMYkgMCiwHEZ4MGIU2JPmmSFQyCc3hnL4iqxvMm3CjLMAHSTUtuYygrs8J5twktpkAwcCYuL27EHJLhgdhUqbvvpRCeDFu0D";
 
 // { autofold 
-module.exports = {myChar: myChar}
+module.exports = {myChar: myChar, position: position}
 //}
