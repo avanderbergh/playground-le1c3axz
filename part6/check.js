46094814706f7926a028dae0704b34e6a3871db9
Adriaan van der Bergh <vanderbergha@VANDERBERGHA-LP01.local>
2019-10-31 10:53:49 +0100

    fixed a bug in checking for part 6

--- part6/check.js
+++ part6/check.js
@@ -4,7 +4,9 @@ let success = true;
 
 const input = "the quick brown fox jumps over the lazy dog";
 
-if (encode(input, 7) != " olgxapjrgiyvcugmvdgqatwzgvblyg olgshfegkvn") success = false;
-if (encode(inout, 20) =! "maytjnbwdtvkhpgtzhqtcnfilthoyktmayteusrtxh ") success = false;
+if (encode(input, 7) != " olgxapjrgiyvcugmvdgqatwzgvblyg olgshfegkvn")
+  success = false;
+if (encode(input, 20) != "maytjnbwdtvkhpgtzhqtcnfilthoyktmayteusrtxh ")
+  success = false;
 
 console.log("TECHIO> success " + String(success));
